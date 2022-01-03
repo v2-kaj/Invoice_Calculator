734b84e5e42c0f13f3fe4fc4b320ddaffc41d6c7
v2-kaj <madalitsomuva@gmail.com>
2022-01-03 14:05:26 +0200

    calculate partly working

--- app/src/main/java/com/example/invoicecalculator/MainActivity.kt
+++ app/src/main/java/com/example/invoicecalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.invoicecalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.invoicecalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateInvoice() }
+    }
+    fun calculateInvoice(){
+        val amount= binding.dailyRate.text.toString().toDouble() * binding.dailyRate.text.toString().toDouble()
+        val formattedAmount = NumberFormat.getCurrencyInstance().format(amount.toString().toDouble())
+        binding.invoiceAmount.text=getString(R.string.invoice_amount_is, formattedAmount)
     }
 }
